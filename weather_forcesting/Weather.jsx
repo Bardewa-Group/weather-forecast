0b4f8d9c1b55a645da3a12c560756b6666909ddb
Sajan Bardewa <105504229+Bardewa-Group@users.noreply.github.com>
2022-09-25 12:35:18 +0545

    Update Weather.jsx

--- weather_forcesting/Weather.jsx
+++ weather_forcesting/Weather.jsx
@@ -39,47 +39,7 @@ const Weather = () => {
         }
     }
 
-    const x = {
-        "coord": {
-            "lon": 73.8553, "lat": 18.5196
-        }
-
-        ,
-        "weather": [{
-            "id": 804, "main": "Clouds", "description": "overcast clouds", "icon": "04n"
-        }
-
-        ],
-        "base": "stations",
-        "main": {
-            "temp": 21.36, "feels_like": 21.82, "temp_min": 21.36, "temp_max": 21.36, "pressure": 1010, "humidity": 87, "sea_level": 1010, "grnd_level": 948
-        }
-
-        ,
-        "visibility": 10000,
-        "wind": {
-            "speed": 2.52, "deg": 257, "gust": 4.13
-        }
-
-        ,
-        "clouds": {
-            "all": 96
-        }
-
-        ,
-        "dt": 1663698730,
-        "sys": {
-            "country": "IN", "sunrise": 1663721595, "sunset": 1663765346
-        }
-
-        ,
-        "timezone": 19800,
-        "id": 1259229,
-        "name": "Pune",
-        "cod": 200
-    }
-
-
+ 
 
 
     useEffect(() => {
